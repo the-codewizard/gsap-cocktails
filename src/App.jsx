a92f742f577f4d54865d6bfb4fb819c442a2af9b
Jai Prakash V <jaiprakash1595@gmail.com>
2026-05-25 21:43:25 +0530

    Implement the about section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 import Cocktails from "./components/Cocktails";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import About from "./components/About";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -12,6 +13,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Cocktails />
+      <About />
     </main>
   );
 };
